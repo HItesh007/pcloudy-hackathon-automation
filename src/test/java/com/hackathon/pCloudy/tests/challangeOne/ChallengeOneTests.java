d8191abb570bfa5d797d932a2e04b2d9e08c43ff
Hitesh Prajapati <hiteshprajapati1992@gmail.com>
2020-06-20 17:59:25 +0530

    ChallengeOneTests: Challenge one completed

---
+++ src/test/java/com/hackathon/pCloudy/tests/challangeOne/ChallengeOneTests.java
@@ -0,0 +1,51 @@
+package com.hackathon.pCloudy.tests.challangeOne;
+
+import com.hackathon.pCloudy.base.SeleniumBase;
+import com.hackathon.pCloudy.enums.Browser;
+import com.hackathon.pCloudy.pages.CPSatExamHomePage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
+
+public class ChallengeOneTests {
+    private static final Logger logger = LogManager.getLogger(ChallengeOneTests.class.getName());
+
+    @Test(priority = 0)
+    public void challengeOneProblemA() {
+        try {
+            SeleniumBase seleniumBase = new SeleniumBase();
+            seleniumBase.initBrowser(Browser.Chrome);
+
+            CPSatExamHomePage homePage = new CPSatExamHomePage();
+            String tamilToEng = homePage
+                    .navigateAndConvertTo("Tamil");
+
+            System.out.println("\n----------------- Following Is Translated From TAMIL -----------------\n");
+            System.out.println(tamilToEng);
+
+            String hindiToEn = homePage
+                    .navigateAndConvertTo("Hindi");
+
+            System.out.println("\n----------------- Following Is Translated From HINDI -----------------\n");
+            System.out.println(hindiToEn);
+
+
+            String kannadaToEn = homePage
+                    .navigateAndConvertTo("Kannada");
+
+            System.out.println("\n----------------- Following Is Translated From KANNADA -----------------\n");
+            System.out.println(kannadaToEn);
+
+            String frenchToEn = homePage
+                    .navigateAndConvertTo("French");
+
+            System.out.println("\n----------------- Following Is Translated From FRENCH -----------------\n");
+            System.out.println(frenchToEn);
+
+            seleniumBase.closeBrowser();
+
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+    }
+}
