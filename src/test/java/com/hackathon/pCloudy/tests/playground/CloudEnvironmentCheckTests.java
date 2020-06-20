af4547b3466276e86f5d7155720569e9683de94d
Hitesh Prajapati <hiteshprajapati1992@gmail.com>
2020-06-20 18:00:19 +0530

    Refactored package names updated on respective classes

--- src/test/java/com/hackathon/pCloudy/tests/playground/CloudEnvironmentCheckTests.java
+++ src/test/java/com/hackathon/pCloudy/tests/playground/CloudEnvironmentCheckTests.java
@@ -1,5 +1,6 @@
-package com.hackathon.pCloudy.tests;
+package com.hackathon.pCloudy.tests.playground;
 
+import com.hackathon.pCloudy.helpers.appium.AppiumSyncHelper;
 import com.hackathon.pCloudy.helpers.report.ReportPortalLogHelper;
 import com.hackathon.pCloudy.thread.ThreadLocalAppiumDriver;
 import com.hackathon.pCloudy.utils.FileUtility;
@@ -8,18 +9,20 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 
-public class LocalEnvironmentCheckTests {
+public class CloudEnvironmentCheckTests {
 
-    private static final Logger logger = LogManager.getLogger(LocalEnvironmentCheckTests.class.getName());
+    private static final Logger logger = LogManager.getLogger(CloudEnvironmentCheckTests.class.getName());
 
     @Test
-    public void checkLocalDeviceSetup() {
-        logger.info("If I am seen in log, which means that the local device setup is working properly.");
+    public void checkCloudDeviceSetup() {
+        logger.info("If I am seen in log, which means that the CLOUD device setup is working properly.");
+        new AppiumSyncHelper().pauseScript(5);
         // Take Screenshot
         ScreenshotUtility screenshotUtility = new ScreenshotUtility();
         byte[] ssAsBytes = screenshotUtility.takeScreenshotAsBase64ByteArray(ThreadLocalAppiumDriver.getAndroidDriver());
         ReportPortalLogHelper
                 .getInstance()
                 .logInfoWithSSAsBase64String(logger, new FileUtility().byteToBase64String(ssAsBytes), "screenshotCaptured");
+
     }
 }
--- src/test/java/com/hackathon/pCloudy/tests/playground/LocalEnvironmentCheckTests.java
+++ src/test/java/com/hackathon/pCloudy/tests/playground/LocalEnvironmentCheckTests.java
@@ -1,6 +1,5 @@
-package com.hackathon.pCloudy.tests;
+package com.hackathon.pCloudy.tests.playground;
 
-import com.hackathon.pCloudy.helpers.appium.AppiumSyncHelper;
 import com.hackathon.pCloudy.helpers.report.ReportPortalLogHelper;
 import com.hackathon.pCloudy.thread.ThreadLocalAppiumDriver;
 import com.hackathon.pCloudy.utils.FileUtility;
@@ -9,20 +8,18 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 
-public class CloudEnvironmentCheckTests {
+public class LocalEnvironmentCheckTests {
 
-    private static final Logger logger = LogManager.getLogger(CloudEnvironmentCheckTests.class.getName());
+    private static final Logger logger = LogManager.getLogger(LocalEnvironmentCheckTests.class.getName());
 
     @Test
-    public void checkCloudDeviceSetup() {
-        logger.info("If I am seen in log, which means that the CLOUD device setup is working properly.");
-        new AppiumSyncHelper().pauseScript(5);
+    public void checkLocalDeviceSetup() {
+        logger.info("If I am seen in log, which means that the local device setup is working properly.");
         // Take Screenshot
         ScreenshotUtility screenshotUtility = new ScreenshotUtility();
         byte[] ssAsBytes = screenshotUtility.takeScreenshotAsBase64ByteArray(ThreadLocalAppiumDriver.getAndroidDriver());
         ReportPortalLogHelper
                 .getInstance()
                 .logInfoWithSSAsBase64String(logger, new FileUtility().byteToBase64String(ssAsBytes), "screenshotCaptured");
-
     }
 }
